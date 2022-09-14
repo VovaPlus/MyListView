dd77cc78dbe5baa7f242a03e77d3a1228319233b
VovaPlus <v.plaun@gmail.com>
2022-09-14 17:20:35 +0300

    FLAG_ACTIVITY_NEW_TASK Kotlin - Этап 0 commit

--- app/src/main/java/com/person/v_plaunov/mylistview/MainActivity.java
+++ app/src/main/java/com/person/v_plaunov/mylistview/MainActivity.java
@@ -1,6 +1,7 @@
 package com.person.v_plaunov.mylistview;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -44,6 +45,34 @@ public class MainActivity extends AppCompatActivity {
 
     //private static String DB_PATH = "/data/data/com.person.v_plaunov.mylistview/databases/";
     //private static String DB_PATH = "/storage/self/primary/My Documents/";
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 
     private static final String DB_NAME = "myDB";
     private static final int REQUEST_CODE_PERMISSION_READ_CONTACTS = 1;
@@ -156,6 +185,7 @@ public class MainActivity extends AppCompatActivity {
                 String strDirPath = tv.getText().toString();
                 String strFilePath = strDirPath + FILENAME;
 
+                verifyStoragePermissions(this);
                 //XmlPullParser xpp = getResources().getXml(R.xml.products);
                 CoinFileParser parser = new CoinFileParser();
                 if(parser.parse(strFilePath))
